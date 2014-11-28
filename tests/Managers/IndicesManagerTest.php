d8efae5cc7afdcce33dc03bd54e2f12a83a42e05
dieterve <dieter@dieterve.be>
2014-11-28 15:39:52 +0100

    Tests: test indicesmanager register.

--- tests/Managers/IndicesManagerTest.php
+++ tests/Managers/IndicesManagerTest.php
@@ -2,13 +2,34 @@
 
 class IndicesManagerTest extends ElasticSearcherTestCase
 {
+	/**
+	 * @var IndicesManager
+	 */
+	private $indicesManager;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->indicesManager = $this->getElasticSearcher()->indicesManager();
+	}
+
 	public function testRegister()
 	{
-		$indicesManager = $this->getElasticSearcher()->indicesManager();
-
 		$moviesIndex = new MoviesIndex();
-		$indicesManager->register($moviesIndex);
 
-		$this->assertEquals(true, $indicesManager->isRegistered('movies'));
+		// Single registration.
+		$this->indicesManager->register($moviesIndex);
+		$this->assertEquals(true, $this->indicesManager->isRegistered('movies'));
+		$this->assertArrayHasKey('movies', $this->indicesManager->registeredIndices());
+		$this->assertInstanceOf(MoviesIndex::class, $this->indicesManager->getRegistered('movies'));
+
+		// Removing from register.
+		$this->indicesManager->unregister('movies');
+		$this->assertEquals(false, $this->indicesManager->isRegistered('movies'));
+		$this->assertArrayNotHasKey('movies', $this->indicesManager->registeredIndices());
+
+		// Bulk registering.
+		$this->indicesManager->registerIndices([$moviesIndex]);
+		$this->assertEquals(true, $this->indicesManager->isRegistered('movies'));
 	}
 }
