b50a340790e947d3e62f3e9aadb78bde07f13a3d
dieterve <dieter@dieterve.be>
2017-08-18 14:27:11 +0200

    Run tests against latest version 5.5.2

--- install-elasticsearch.sh
+++ install-elasticsearch.sh
@@ -3,7 +3,7 @@
 echo ">>> Installing Elasticsearch"
 
 # Set some variables
-ELASTICSEARCH_VERSION=5.1.2 # Check http://www.elasticsearch.org/download/ for latest version
+ELASTICSEARCH_VERSION=5.5.2 # Check http://www.elasticsearch.org/download/ for latest version
 
 # Install prerequisite: Java8
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
@@ -19,7 +19,6 @@ rm elasticsearch-$ELASTICSEARCH_VERSION.deb
 # Configure Elasticsearch for development purposes (1 shard/no replicas, don't allow it to swap at all if it can run without swapping)
 sudo sed -i "s/#index.number_of_shards: 1/index.number_of_shards: 1/" /etc/elasticsearch/elasticsearch.yml
 sudo sed -i "s/#index.number_of_replicas: 0/index.number_of_replicas: 0/" /etc/elasticsearch/elasticsearch.yml
-sudo sed -i "s/#bootstrap.memory_lock: true/bootstrap.memory_lock: true/" /etc/elasticsearch/elasticsearch.yml
 if [ "$1" != "testing" ] ; then
 	echo ">>> Binding elasticsearch to all network hosts"
 	sudo sed -i "s/#network.host: 192.168.0.1/network.host: 0.0.0.0/" /etc/elasticsearch/elasticsearch.yml
